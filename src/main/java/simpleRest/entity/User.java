b2a818109ceeea38369cedfea6d842815a97a115
Anna Matveyeva <matveyeva16563@gmail.com>
2019-12-23 12:43:34 +0300

    Add H2 database and user entity

---
+++ src/main/java/simpleRest/entity/User.java
@@ -0,0 +1,19 @@
+package simpleRest.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String name;
+
+    private String password;
+}
