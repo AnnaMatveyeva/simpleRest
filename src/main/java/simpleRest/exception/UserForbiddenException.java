5bc17d6eab744137b22c2c842223594351b324cf
Anna Matveyeva <matveyeva16563@gmail.com>
2019-12-27 12:41:08 +0300

    Delete ResponseStatus annotation

--- src/main/java/simpleRest/exception/UserForbiddenException.java
+++ src/main/java/simpleRest/exception/UserForbiddenException.java
@@ -1,9 +1,5 @@
 package simpleRest.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "Bad credentials")
 public class UserForbiddenException extends Exception {
 
     public UserForbiddenException(String message) {
